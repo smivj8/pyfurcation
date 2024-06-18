780ebfa9fa2d80a36bfaec9242c91f3ed67a0eb8
smivj8 <samuel.smith@cchmc.org>
2024-06-18 14:43:05 -0400

    messed around with the axial rotations

--- max_generation_testing.py
+++ max_generation_testing.py
@@ -14,25 +14,28 @@ with open("lung_dimensions_mm.csv") as fp:
     data_read = [row for row in reader]
 
 lung_dimensions = np.array(data_read).astype(np.float64)
-lung_dimensions[:,1:] = lung_dimensions[:,1:] * 1E3
+lung_dimensions[:,1:] = lung_dimensions[:,1:]
 n_gens = np.shape(lung_dimensions)[0]
 
 
-branching_angle_tes = 35*np.pi/180
+branching_angle_test = 35*np.pi/180
 carina_angle_test = 3.6*np.pi/180
 
 lung_parameters = [0] * (n_gens - 1)
 axial_rotations = [0] * (n_gens - 1)
+# axial_rotations = np.random.rand(n_gens - 1) * np.pi * 2
+# 
 
 for j in range(1, n_gens):
     parent_radius_j = lung_dimensions[j-1, 1]
     daughter_radius_j = lung_dimensions[j,1]
     daughter_length_j = lung_dimensions[j,2]
     outer_radius_j = daughter_radius_j * 5
-    gen_j_parameters = [parent_radius_j, 0, daughter_radius_j, daughter_length_j, outer_radius_j ,branching_angle_tes, carina_angle_test, 1]
+    gen_j_parameters = [parent_radius_j, 0, daughter_radius_j, daughter_length_j, outer_radius_j ,branching_angle_test, carina_angle_test, 1]
     lung_parameters[j-1] = gen_j_parameters
-    axial_rotations[j-1] = np.pi/2 * np.mod(j+1, 2)
+    axial_rotations[j-1] = np.pi * np.mod(j-1, 2 ) + np.pi/2 + (np.random.rand()*np.pi/2 - (np.pi/4))
 
+axial_rotations[0] = 0
 n_min = 0
 
 bifurcating_tree = single_pathway_bifurcating_model(lung_parameters[n_min:], axial_rotations[n_min:])
